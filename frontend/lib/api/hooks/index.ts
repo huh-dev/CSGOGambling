947c28f48289a3764fde60b12fdefa74d6b2908d
christianbuchberg <cb@personalezonen.dk>
2026-03-13 14:39:21 +0100

    removed the nextjs frontend

--- frontend/lib/api/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { useInventory, useInventoryItem } from "./useInventory"
-export type { UseInventoryReturn, UseInventoryItemReturn } from "./useInventory"
-
-export { useUser, useSteamLogin, useLogout } from "./useAuth"
-export type { AuthUser, UseUserReturn, UseLoginReturn, UseLogoutReturn } from "./useAuth"
